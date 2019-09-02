8ec80ae68d099eb5bd401c8657667aa875c1add2
kyoyaiida@naver.com <ijinc123!@#>
2019-09-02 13:41:14 +0900

    로그인 작업중

---
+++ src/main/java/inje/bimclient/bim/admin/web/InterfaceAdminController.java
@@ -0,0 +1,12 @@
+package inje.bimclient.bim.admin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class InterfaceAdminController {
+	@RequestMapping("/admin/index")
+	public String index() {
+		return "test";
+	}
+}
