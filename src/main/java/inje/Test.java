5f5bec320c32da9f74e392ff00ec1637b741b3f8
kyoyaiida@naver.com <ijinc123!@#>
2019-09-19 13:49:59 +0900

    미사용 클래스 삭제

--- src/main/java/inje/Test.java
+++
@@ -1,9 +0,0 @@
-package inje;
-
-import inje.user.LoginUserDetail;
-import org.bimserver.client.BimServerClient;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-public class Test {
-    public static BimServerClient bimServerClient = ((LoginUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getBimServerClient();
-}
